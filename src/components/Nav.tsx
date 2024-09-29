afe61f7897efcab54a36b4f841463cce63250718
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-29 01:26:03 -0400

    completed nav bar and add necessary links to pages.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  // navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    // <div>Nav</div>
+    <nav className='nav'>
+      <ul>
+        <Link to="/" className='nav-item'>
+          Home
+        </Link>
+        <Link to="/SavedCandidates" className='nav-item'>
+          Potential Candidates
+        </Link>
+      </ul>
+    </nav>
   )
 };
 
