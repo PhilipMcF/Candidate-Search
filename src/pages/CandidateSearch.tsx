2dd54bcea33ff1bee847fa5eba7c597c824732cd
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-29 02:25:18 -0400

    working on atm

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,5 +1,24 @@
 // import { useState, useEffect } from 'react';
-// import { searchGithub, searchGithubUser } from '../api/API';
+import { searchGithub } from '../api/API';
+import { searchGithubUser } from '../api/API';
+
+async function main(){
+  const candidates = await searchGithub();
+
+  let potentialCandidates = [];
+  for (const candidate of candidates) {
+    const login = candidate.login;
+    const loginResponse = await searchGithubUser(login);
+
+    if (loginResponse){
+      potentialCandidates.push(loginResponse);
+    }
+    // console.log(loginResponse);
+  }
+  console.log(potentialCandidates);
+}
+
+main();
 
 const CandidateSearch = () => {
   return <h1>CandidateSearch</h1>;
