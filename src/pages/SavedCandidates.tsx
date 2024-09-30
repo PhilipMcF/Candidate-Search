f41a6c4832d1cb2bd616c3c8d28b21310013b32c
PhilipMcFDesktop <philipsm1998@gmail.com>
2024-09-29 22:44:06 -0400

    finished onClick handler for reject button

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 const SavedCandidates = () => {
   const storedCandidates = localStorage.getItem('savedCandidates');
   const candidateList = JSON.parse(storedCandidates || '[]');
-  console.log(candidateList);
+
+  const [candidates, setCandidates] = useState(candidateList); 
+
   
+  const handleReject = (login: string) => {
+    const updatedCandidates = candidates.filter(
+      (candidate: { login: string }) => candidate.login !== login
+    );
+    
+    setCandidates(updatedCandidates);
+    localStorage.setItem('savedCandidates', JSON.stringify(updatedCandidates));
+
+    console.log(`Candidate with login: ${login} has been rejected`);
+  };
 
   return (
     <>
@@ -24,12 +38,12 @@ const SavedCandidates = () => {
           (candidate: { avatar_url: any; name: any; login: any; location: any; email: any; company: any; bio: any; }) => (
             <tr key={candidate.login}>
               <td className="tableImage"><img src={candidate.avatar_url} alt="Avatar" className="tableAvatar" /></td>
-              <td>{`${candidate.name} (${candidate.login})` || "Unknown"}</td>
+              <td>{candidate.name ? `${candidate.name} (${candidate.login})` : candidate.login || "Unknown"}</td>
               <td>{candidate.location || "Unknown"}</td>
               <td>{candidate.email || "N/A"}</td>
               <td>{candidate.company || "N/A"}</td>
               <td>{candidate.bio || "N/A"}</td>
-              <td className="tableRejectButton"><button className="rejectButton">X</button></td>
+              <td className="tableRejectButton"><button className="rejectButton" onClick={() => handleReject(candidate.login)}>X</button></td>
             </tr>
           )
         )}
